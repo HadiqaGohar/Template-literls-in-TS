3c19d7f7c2c8677a6d4c367f8ea2ec0ba67ec84e
HadiqaGohar <160311280+HadiqaGohar@users.noreply.github.com>
2024-03-26 05:01:51 -0700

    $

---
+++ main.js
@@ -0,0 +1,10 @@
+//TEMPLETE LITERLAS
+//QUESTION 1
+var firstName = "Hadiqa";
+var lastName = "Gohar";
+var fullName = "".concat(firstName, " ").concat(lastName);
+console.log(fullName);
+//QUESTION 2
+var car = "Range Rover";
+var message = "I like ".concat(car);
+console.log(message);
---
+++ main.ts
@@ -0,0 +1,16 @@
+//TEMPLETE LITERLAS
+
+//QUESTION 1
+
+let firstName : string = "Hadiqa";
+let lastName : string = "Gohar";
+let fullName : string = `${firstName} ${lastName}`
+console.log (fullName)
+
+//QUESTION 2
+
+let car : string = "Range Rover";
+let message : string = `I like ${car}`
+console.log (message);
+
+
